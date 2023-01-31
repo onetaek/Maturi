03009a778848fde6253a71bb090b343fc18b0c7a
hyeahyun2 <hgass66@naver.com>
2023-01-31 13:19:58 +0900

    first commit

---
+++ src/main/java/com/maturi/MaturiApplication.java
@@ -0,0 +1,13 @@
+package com.maturi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaturiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaturiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maturi/MaturiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maturi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaturiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
