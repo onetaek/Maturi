5e2a030f0e6ae3f4609dfa5c39d7af9a70a888e0
oh one teak <dnjsxorchlrh@naver.com>
2023-03-09 02:35:05 +0900

    색상 살짝변경 Controller 매핑 수정

--- src/main/java/com/maturi/controller/TestController.java
+++ src/main/java/com/maturi/controller/TestController.java
@@ -2,13 +2,14 @@ package com.maturi.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HelloController {
-
-    @GetMapping("/hello")
-    public String hello(){
-        return "hello";
+@Controller
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping("/login")
+    public String login(){
+        return "login";
     }
 }
