16605e00b87328dcb4d8e127da7fcae929c04479
oh one teak <dnjsxorchlrh@naver.com>
2023-03-15 00:35:39 +0900

    spring-validation 적용중

--- src/main/java/com/maturi/controller/MemberController.java
+++ src/main/java/com/maturi/controller/MemberController.java
@@ -1,15 +1,16 @@
 package com.maturi.controller;
 
 import com.maturi.dto.member.MemberJoinDTO;
+import com.maturi.dto.member.MemberJoinRequest;
 import com.maturi.entity.member.Member;
 import com.maturi.service.member.MemberService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @AllArgsConstructor
@@ -19,14 +20,38 @@ public class MemberController {
 
   final private MemberService memberService;
   @GetMapping("/join")
-  public String getJoin(){
+  public String getJoin(Model model){
+    model.addAttribute("member",new MemberJoinDTO());
     return "/member/join";
   }
   @PostMapping("/join")
-  public String postJoin(@ModelAttribute MemberJoinDTO memberJoinDTO){
-
+  public String postJoin(
+          @ModelAttribute MemberJoinDTO memberJoinDTO,
+          @RequestParam(name = "pwCheck") String pwCheck
+  ){
     memberService.join(memberJoinDTO);
+    return "redirect:/member/login";
+  }
 
+//  @PostMapping("/join")
+  public String joinValidation(@Validated @ModelAttribute(name = "member") MemberJoinRequest memberJoinRequest,
+                               BindingResult bindingResult){
+    log.info("validation join start");
+    log.info("memberJoinRequest = {}",memberJoinRequest.toString());
+    String passwd = memberJoinRequest.getPasswd();
+    String passwdCheck = memberJoinRequest.getPasswdCheck();
+    if(passwd!=null && passwdCheck!=null){
+      if(passwd != passwdCheck){
+        bindingResult.reject("passwordCheck","비밀번호 불일치 기본 메시지");
+      }
+    }
+    //검증에 실패하면 다시 입력 폼으로
+    if (bindingResult.hasErrors()) {
+      log.info("errors={} ", bindingResult);
+      return "redirect:/member/join";
+    }
+
+//    memberService.join(memberJoinDTO);
     return "redirect:/member/login";
   }
 
--- src/main/java/com/maturi/dto/member/MemberJoinDTO.java
+++ src/main/java/com/maturi/dto/member/MemberJoinDTO.java
@@ -1,14 +1,19 @@
 package com.maturi.dto.member;
 
-import com.maturi.entity.member.Member;
 import com.maturi.entity.member.MemberStatus;
 import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class MemberJoinDTO {
+
   private String email;
   private String passwd;
   private String salt;
