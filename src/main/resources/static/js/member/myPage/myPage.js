acffe9eee3a3fa1982c1231ab66f6e584cae4319
hyeahyun2 <hgass66@naver.com>
2023-03-24 10:42:09 +0900

    editMyPage 스크립트 수정

--- src/main/resources/static/js/member/myPage/myPage.js
+++ src/main/resources/static/js/member/myPage/myPage.js
@@ -1,17 +1,27 @@
-const moreBtn = document.querySelector(".moreBtn");
-const memberBtnUl = document.querySelector(".memberBtn ul");
+/* 더보기 버튼 */
+const ellipsisBtn = document.querySelectorAll(".ellipsis-btn");
+const ellipsisContent = document.querySelectorAll(".ellipsis-content");
 
-let ulDisplay = "none"; // 초기값 설정
-moreBtn.addEventListener("click", ()=>{
+for(let i = 0 ; i < ellipsisBtn.length; i++){
+  ellipsisBtn[i].addEventListener("click",function(){
+    ellipsisContent[i].classList.toggle("active");
+  });
+}
 
-  if(ulDisplay == 'none'){
-    ulDisplay = "block";
-  }
-  else {
-    ulDisplay = "none";
-  }
-  memberBtnUl.style.display = ulDisplay;
-});
+// const moreBtn = document.querySelector(".moreBtn");
+// const memberBtnUl = document.querySelector(".memberBtn ul");
+//
+// let ulDisplay = "none"; // 초기값 설정
+// moreBtn.addEventListener("click", ()=>{
+//
+//   if(ulDisplay == 'none'){
+//     ulDisplay = "block";
+//   }
+//   else {
+//     ulDisplay = "none";
+//   }
+//   memberBtnUl.style.display = ulDisplay;
+// });
 
 /* 프로필 편집 버튼 클릭 */
 const editMyPage = document.querySelector(".editMyPage");
